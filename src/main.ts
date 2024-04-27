4161817d3837a88c0340495872930911280bc885
imsfc <imsfc9@gmail.com>
2024-04-27 21:12:03 +0800

    太空电梯

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { getMilestones } from './milestone'
+import { getPhases } from './phases'
 import { saveFile } from './util'
 
-const milestones = await getMilestones()
+const phases = await getPhases()
+saveFile('phases.json', JSON.stringify(phases))
 
+const milestones = await getMilestones()
 saveFile('milestones.json', JSON.stringify(milestones))
