aadc0008587cbd956dbdb7e62a679a1d1a949e77
imsfc <imsfc9@gmail.com>
2024-04-27 23:42:21 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { getMilestones } from './milestone'
 import { getPhases } from './phases'
 import { saveFile } from './util'
 
-// const phases = await getPhases()
-// saveFile('phases.json', JSON.stringify(phases))
+const phases = await getPhases()
+saveFile('phases.json', JSON.stringify(phases))
 
-// const milestones = await getMilestones()
-// saveFile('milestones.json', JSON.stringify(milestones))
+const milestones = await getMilestones()
+saveFile('milestones.json', JSON.stringify(milestones))
 
 const [buildings, recipes] = await getBuildings()
 saveFile('buildings.json', JSON.stringify(buildings))
